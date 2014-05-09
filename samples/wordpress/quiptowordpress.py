895316c506a6f9b79c5db1cbae157b6514938517
Casey Muller <casey@quip.com>
2014-05-08 21:24:58 -0700

    Add image support to WordPress example

--- samples/wordpress/quiptowordpress.py
+++ samples/wordpress/quiptowordpress.py
@@ -21,11 +21,13 @@ This is a sample app for the Quip API - https://quip.com/api/.
 
 import argparse
 import quip
+import xml.etree.cElementTree
 import xmlrpclib
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Backup of a Quip account")
+    parser = argparse.ArgumentParser(
+        description="Post Quip threads to WordPress")
     parser.add_argument("--quip_access_token", required=True,
         help="Access token for your Quip account")
     parser.add_argument("--wordpress_xmlrpc_url", required=True,
@@ -44,8 +46,32 @@ def main():
     server = xmlrpclib.ServerProxy(args.wordpress_xmlrpc_url)
     threads = client.get_threads(args.thread_ids)
     for thread in threads.values():
-        # Remove the title from the top of the document HTML
-        html = thread["html"][thread["html"].index("</h1>") + 5:].strip()
+        # Parse the document
+        tree = client.parse_document_html(thread["html"])
+        # Upload each image to wordpress and replace with the new URL
+        for img in tree.iter("img"):
+            src = img.get("src")
+            if not src.startswith("/blob"):
+                continue
+            _, _, thread_id, blob_id = src.split("/")
+            blob_response = client.get_blob(thread_id, blob_id)
+            mimetype = blob_response.info().get("Content-Type")
+            result = server.wp.uploadFile(
+                0, args.wordpress_username, args.wordpress_password, {
+                    "name": "%s.%s" % (blob_id, mimetype.split("/")[-1]),
+                    "type": mimetype,
+                    "bits": xmlrpclib.Binary(blob_response.read()),
+                    "overwrite": True,
+                })
+            img.set("src", result["url"])
+        # Remove the title element to avoid repeating it
+        for child in tree:
+            if child.text == thread["thread"]["title"]:
+                tree.remove(child)
+                break
+        html = unicode(xml.etree.cElementTree.tostring(tree))
+        # Strip the <html> tags that were introduced in parse_document_html
+        html = html[6:-7]
         post_id = server.wp.newPost(
             0, args.wordpress_username, args.wordpress_password, {
                 "post_title": thread["thread"]["title"],
@@ -54,7 +80,6 @@ def main():
         if args.publish:
             server.mt.publishPost(
                 post_id, args.wordpress_username, args.wordpress_password)
-
 
 if __name__ == '__main__':
     main()
